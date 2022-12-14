44e549b94634758a186e7c34116a1099f7c02633
m0rpheus <fotshudi@gmail.com>
2022-12-14 18:01:45 +0100

    Update backgroundProvider.js

--- common/backgroundProvider.js
+++ common/backgroundProvider.js
@@ -1,21 +1,24 @@
-const defaultBackgrounds = require("./defaultBackgrounds.js") ; 
+const defaultBackgrounds = require("./defaultBackgrounds.js");
 
 const { UNSPLASH_API_KEY } = process.env;
 const { BACKGROUND_DISPLAY_MINUTES, BACKGROUND_FETCH_INTERVAL_MINUTES, BACKGROUND_TOPICS } = process.env;
-const DEBUG = process.env.LOGLEVEL=="debug";
+const DEBUG = process.env.LOGLEVEL == "debug";
 
 
-var bgArray = defaultBackgrounds.list;
+var bgArray = []; //defaultBackgrounds.list;
 var currentBg = null;
 var lastRenewTime = new Date(1999, 11, 16);
 var lastApiCall = new Date(1999, 11, 16);
 const topics = BACKGROUND_TOPICS.split(',');
 
 
-const providerUrl = `https://api.unsplash.com/photos/random?query=_QUERY_&count=6&client_id=${UNSPLASH_API_KEY}`
+const providerUrl = `https://api.unsplash.com/photos/random?query=_QUERY_&count=3&client_id=${UNSPLASH_API_KEY}`
 
 
 const fetchNew = () => {
+
+    if (bgArray && bgArray.length > 20) { if (DEBUG) console.log("Not feteching from unsspash, there are enough items in cache", bgArray.length); return ; }
+
 
     const fetch = require('node-fetch');
     const elapsedMinutes = getElapsedMinutes(lastApiCall);
@@ -30,20 +33,23 @@ const fetchNew = () => {
         fetch(queryUrl, settings)
             .then(res => {
                 try {
-                    let ret = res.json() ;
+                    let ret = res.json();
                     if (DEBUG) console.log("AA - Received from unsplash json", ret);
                     return ret;
                 }
                 catch (f) {
                     console.log("ERROR", f, "using default image list");
                     console.error(f);
-                    return defaultBackgrounds.list ;
+                    return defaultBackgrounds.list;
                     // bgArray = defaultBackgrounds.list ;
                 }
             }
             ).then((json) => {
                 if (DEBUG) console.log("BB - setting bgArray ", json);
-                bgArray = json;
+                bgArray =  bgArray ? bgArray.concat(json) : json;
+
+                // shuffle items
+                bgArray = bgArray.sort((a, b) => 0.5 - Math.random());
             });
     }
     else {
@@ -64,11 +70,11 @@ function getCurrentBackground() {
 
     const elapsedMinutes = getElapsedMinutes(lastRenewTime);
 
-    currentBg = null == currentBg ? defaultBackgrounds.list[0] : currentBg ; 
+    currentBg = null == currentBg ? defaultBackgrounds.list[0] : currentBg;
 
     try {
 
-        bgArray = undefined == bgArray || null == bgArray ?  defaultBackgrounds.list : bgArray ;
+        bgArray = undefined == bgArray || null == bgArray ? defaultBackgrounds.list : bgArray;
 
         if ((!currentBg || elapsedMinutes > Number(BACKGROUND_DISPLAY_MINUTES)) && bgArray.length > 0) {
             lastRenewTime = new Date();
@@ -90,7 +96,7 @@ function getCurrentBackground() {
 
 fetchNew();
 
-setInterval(fetchNew, 15 * 60 * 1000),
+setInterval(fetchNew, 5 * 60 * 1000),
 
 
 module.exports = { getCurrentBackground };
